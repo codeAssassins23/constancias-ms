1579884a4db03f7ae0fc2f068b0dff41904717c0
codeAssassins23 <bravovictorhugo11@gmail.com>
2024-12-19 12:25:07 -0500

    implementacion de middleware aws-xray 60%

--- src/infrastructure/config/environments/xray.ts
+++ src/infrastructure/config/environments/xray.ts
@@ -2,8 +2,8 @@ import * as AWSXRay from 'aws-xray-sdk';
 import { envs } from './envs';
 
 export function configureXRay() {
-  const deamonAddress = envs.xray.daemonAddress;
-  AWSXRay.setDaemonAddress(deamonAddress);
+  const daemonAddress = envs.xray.daemonAddress;
+  AWSXRay.setDaemonAddress(daemonAddress);
 
   AWSXRay.captureHTTPsGlobal(require('http'));
   AWSXRay.captureHTTPsGlobal(require('https'));
@@ -11,4 +11,6 @@ export function configureXRay() {
   if (envs.nodeEnv === 'production') {
     AWSXRay.config([AWSXRay.plugins.EC2Plugin]);
   }
+
+  AWSXRay.middleware.setDefaultName('(NRO)-ms-constancias');
 }
